e533f4332b5efd822d15ba9f846ba2cfd7841635
Eddie Pace <eddie@pace.me.uk>
2026-02-11 09:57:47 +0000

    remove old test

--- client/src/blockchain/blockchain.spec.ts
+++ client/src/blockchain/blockchain.spec.ts
@@ -12,11 +12,4 @@ describe('Blockchain', () => {
     const fixture = TestBed.createComponent(Blockchain);
     expect(fixture.componentInstance).toBeTruthy();
   });
-
-  it('should render placeholder text', async () => {
-    const fixture = TestBed.createComponent(Blockchain);
-    await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('p')?.textContent).toContain('Blockchain component works!');
-  });
 });
