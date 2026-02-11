68ff93e01dd376e38d6c83912685ac917b6b8fab
Eddie Pace <eddie@pace.me.uk>
2026-02-11 14:25:10 +0000

    no-implicit-override: false

--- client/src/common/service-error.ts
+++ client/src/common/service-error.ts
@@ -1,12 +1,10 @@
 import { catchError } from 'rxjs';
 
 export class ServiceError extends Error {
-  readonly originalCause?: unknown;
-
   constructor(message: string, cause?: unknown) {
     super(message);
     this.name = 'ServiceError';
-    this.originalCause = cause;
+    this.cause = cause;
   }
 }
 
